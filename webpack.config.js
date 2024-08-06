dcf9f44ec45e6ed011b17c4c9c269788f0ea2f1b
pparkssang93 <pparkssang1013@gmail.com>
2024-08-06 23:57:45 +0900

    Fix_ #1, #2
    Close #1, Close #2

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 import path from "path";
 
+const __dirname = path.resolve();
+
 export default {
-    entry: "script.js",
+    entry: "./script.js",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
