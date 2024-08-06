dfd6f42bcc173a7403bb037b999b40876fd05862
pparkssang93 <pparkssang1013@gmail.com>
2024-08-07 01:02:15 +0900

    Feat_ script.js 파일 생성
    - webpack 테스트 코드
    
    Close #3

---
+++ script.js
@@ -0,0 +1,8 @@
+function component() {
+    const element = document.createElement("div");
+
+    element.innerHTML = "Hello world";
+    return element;
+}
+
+document.body.appendChild(component());
